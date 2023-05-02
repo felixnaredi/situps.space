6c9919d76e9de03f1abe081207b8f54b37de29af
felixnaredi <naredi@student.chalmers.se>
2023-05-02 08:29:38 +0200

    Added tests for url parsing

--- server/src/api/get_room_properties.rs
+++ server/src/api/get_room_properties.rs
@@ -75,6 +75,8 @@ pub fn handle() -> impl warp::Filter
 #[cfg(test)]
 mod test
 {
+    use std::str::FromStr;
+
     use hyper::{
         Client,
         Uri,
@@ -101,7 +103,7 @@ mod test
             warp::path!("api" / "room" / "get-room-properties")
                 .and(warp::query::<Base64EncodedRequest<GetRoomPropertiesRequest>>())
                 .map(move |request| {
-                    println!("server received request: {:?}", request);
+                    println!("server received: {:?}", request);
                     assert_eq!(request, value);
                     warp::reply()
                 }),
@@ -113,38 +115,101 @@ mod test
         (tx, tokio::spawn(server))
     }
 
-    #[tokio::test]
-    async fn requests_are_parsed_correctly()
+    fn send_request(port: u32, url_parameters: &str) -> hyper::client::ResponseFuture
     {
-        let request = GetRoomPropertiesRequest {
-            room_id: 1,
-            dates: vec![
-                GregorianScheduleDate::new(2023, 4, 29),
-                GregorianScheduleDate::new(2023, 4, 30),
-                GregorianScheduleDate::new(2023, 5, 1),
-            ],
-            entries: false,
-            users: false,
-            display_name: false,
-            url: false,
-            broadcast: false,
-        };
-
-        let (tx, server) = server_expecting_request(8001, Base64EncodedRequest(request));
-
         let client = Client::new();
-        let res = client
-            .get(Uri::from_static(
-                "http://127.0.0.1:8001/api/room/get-room-properties?b64=eyJyb29tSWQiOjEsImRhdGVzIjpbeyJ5ZWFyIjoyMDIzLCJtb250aCI6NCwiZGF5IjoyOX0seyJ5ZWFyIjoyMDIzLCJtb250aCI6NCwiZGF5IjozMH0seyJ5ZWFyIjoyMDIzLCJtb250aCI6NSwiZGF5IjoxfV0sImVudHJpZXMiOmZhbHNlLCJ1c2VycyI6ZmFsc2UsImRpc3BsYXlOYW1lIjpmYWxzZSwidXJsIjpmYWxzZSwiYnJvYWRjYXN0IjpmYWxzZX0",
+        client.get(
+            Uri::from_str(&format!(
+                "http://127.0.0.1:{}/api/room/get-room-properties?{}",
+                port, url_parameters
             ))
+            .unwrap(),
+        )
+    }
+
+    #[tokio::test]
+    async fn parse_generated_requests()
+    {
+        use rand::Rng;
+
+        let mut rng = rand::thread_rng();
+
+        for size in [(0..=0), (1..=1), (2..=4), (2..=4), (20..=40), (80..=160)] {
+            //
+            // Generate request.
+            //
+            let request = GetRoomPropertiesRequest {
+                room_id: rng.gen(),
+                dates: (0..rng.gen_range(size))
+                    .into_iter()
+                    .map(|_| {
+                        GregorianScheduleDate::new(
+                            rng.gen_range(1000..3000),
+                            rng.gen_range(1..=12),
+                            rng.gen_range(1..28),
+                        )
+                    })
+                    .collect(),
+                entries: rng.gen(),
+                users: rng.gen(),
+                display_name: rng.gen(),
+                url: rng.gen(),
+                broadcast: rng.gen(),
+            };
+            let request = Base64EncodedRequest(request);
+
+            //
+            // Launch server.
+            //
+            let (tx, server) = server_expecting_request(8000, request.clone());
+
+            //
+            // Send url encoded request to the server and check that it was successful.
+            //
+            let response = send_request(8000, &serde_urlencoded::to_string(&request).unwrap())
+                .await
+                .unwrap();
+            assert!(response.status().is_success());
+
+            //
+            // Close down server.
+            //
+            tx.send(()).unwrap();
+            server.await.unwrap();
+        }
+    }
+
+    #[tokio::test]
+    async fn parse_request_with_only_room_id()
+    {
+        //
+        // Launch server.
+        //
+        let (tx, server) = server_expecting_request(
+            8001,
+            Base64EncodedRequest(GetRoomPropertiesRequest {
+                room_id: 17956551056096027663,
+                dates: vec![],
+                entries: false,
+                users: false,
+                display_name: false,
+                url: false,
+                broadcast: false,
+            }),
+        );
+
+        //
+        // Send url encoded request to the server and check that it was successful.
+        //
+        let response = send_request(8001, "b64=eyJyb29tSWQiOjE3OTU2NTUxMDU2MDk2MDI3NjYzfQo")
             .await
             .unwrap();
-        println!("status: {}", res.status());
-        println!("body: {:?}", res.body());
+        assert!(response.status().is_success());
 
-        assert!(res.status().is_success());
-
+        //
+        // Close down server.
+        //
         tx.send(()).unwrap();
-        server.await.ok();
+        server.await.unwrap();
     }
 }
